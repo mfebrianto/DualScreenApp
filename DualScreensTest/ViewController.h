20ffa814364fe46c5680f8f539f9854f9d564d6a
mfebrianto <michaelfebrianto@gmail.com>
2015-07-19 18:01:40 +1000

    Initial Commit

---
+++ DualScreensTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DualScreensTest
+//
+//  Created by michael febrianto on 19/07/2015.
+//  Copyright (c) 2015 michael febrianto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DualScreensTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DualScreensTest
+//
+//  Created by michael febrianto on 19/07/2015.
+//  Copyright (c) 2015 michael febrianto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
